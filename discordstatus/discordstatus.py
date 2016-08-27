47158b7f3d9d5f24cd619cb2c6a81350a64817d1
Pepsiman <thesamppa1@gmail.com>
2016-08-27 22:51:15 +0300

    Added Discord status checker
    Added Discord status checker cog

---
+++ discordstatus/discordstatus.py
@@ -0,0 +1,36 @@
+import discord
+from discord.ext import commands
+import aiohttp
+
+try:
+    from bs4 import BeautifulSoup
+    soupAvailable = True
+except:
+    soupAvailable = False
+
+
+class DiscordStatus:
+    """Get Discord status"""
+
+    def __init__(self, bot):
+        self.bot = bot
+
+    @commands.command()
+    async def status(self):
+        """Get Discord Status"""
+
+        url = "https://status.discordapp.com/"
+        async with aiohttp.get(url) as response:
+            soupObject = BeautifulSoup(await response.text(), 'html.parser')
+            try:
+                status = soupObject.find(class_='unresolved-incident impact-none').get_text()
+                await self.bot.say(status)
+            except:
+                await self.bot.say("Discord: Everything is fine")
+
+
+def setup(bot):
+    if soupAvailable:
+        bot.add_cog(DiscordStatus(bot))
+    else:
+        raise RuntimeError("You need to run `pip3 install beautifulsoup4`")
