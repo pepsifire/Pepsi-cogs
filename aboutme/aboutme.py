44507445d4f6154c6a479ceb2a657a943e3618cf
Pepsiman <thesamppa1@gmail.com>
2016-11-27 14:08:29 +0200

    Added embed

--- aboutme/aboutme.py
+++ aboutme/aboutme.py
@@ -1,26 +1,37 @@
+import discord
 from discord.ext import commands
 
 
 class AboutMe:
-    """Tells stuff about you"""
+    """Tells stuff about people"""
 
     def __init__(self, bot):
         self.bot = bot
 
     @commands.command(pass_context=True)
-    async def aboutme(self, ctx):
-        """Tells stuff about you"""
-        name = ctx.message.author.name
-        userid = ctx.message.author.id
-        bot = str(ctx.message.author.bot)
-        join_date = ctx.message.author.created_at
-        top_role = ctx.message.author.top_role
-        await self.bot.say("```" + "\nInfo about: " + name + "\n"
-                           + "ID: " + userid + "\n" +
-                           "Are you a bot?: " + bot + "\n"
-                           + "You joined at: " + str(join_date) + "\n"
-                           + "Your highest role is: " + str(top_role)
-                           + "\n```")
+    async def aboutme(self, ctx, user: discord.Member=None):
+        """Tells stuff about people"""
+        author = ctx.message.author
+        if not user:
+            user = author
+
+        name = user.name
+        userid = user.id
+        if user.bot is False:
+            bot = "Nope"
+        else:
+            bot = "Beep Boop"
+        join_date = user.created_at.strftime("%d %b %Y")
+        top_role = user.top_role
+        data = discord.Embed(title="")
+        data.set_author(name="{}".format(user), icon_url=user.avatar_url)
+        data.add_field(name="Name", value=name)
+        data.add_field(name="User ID", value=userid)
+        data.add_field(name="Am i a bot?", value=bot)
+        data.add_field(name="Discord Join Date", value=join_date)
+        data.add_field(name="Top Role", value=top_role)
+        data.set_thumbnail(url=user.avatar_url)
+        await self.bot.say(embed=data)
 
 
 def setup(bot):
